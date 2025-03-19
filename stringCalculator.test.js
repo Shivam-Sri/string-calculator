244abb1d5c0dd540b3f1bb1d28749759630370c8
shivam.srivastava <shivam.srivastava@codvo.ai>
2025-03-19 09:53:02 +0530

    test case#2 - number check for single number

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,7 +1,10 @@
-const { add } = require('./stringCalculator');
- 
- describe('String Calculator', () => {
-   test('should return 0 for empty string', () => {
-     expect(add('')).toBe(0);
-   });
- }); 
+const { add } = require("./stringCalculator");
+
+describe("String Calculator", () => {
+  test("should return 0 for empty string", () => {
+    expect(add("")).toBe(0);
+  });
+  test("should return number for single number", () => {
+    expect(add("1")).toBe(1);
+  });
+});
