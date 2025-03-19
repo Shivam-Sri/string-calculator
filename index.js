ecb519f7e1b5122e6d6cd5c72380b2e310e1737c
shivam.srivastava <shivam.srivastava@codvo.ai>
2025-03-19 09:44:23 +0530

    reverting push

--- index.js
+++
@@ -1,6 +0,0 @@
-const { add } = require('./stringCalculator');
-   
-// Examples
-console.log(add('1,2,3')); // Output: 6
-console.log(add('//;\n1;2;3')); // Output: 6
-console.log(add('//[***]\n1***2***3')); // Output: 6
